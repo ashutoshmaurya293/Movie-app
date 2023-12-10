ac8b5e0c8197471d2c7d8cd3b95a96fd905e5fc6
Ashutosh Maurya <ashutoshmaurya079@gmail.com>
2023-12-10 18:27:56 +0530

    adding components section

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
 import './App.css'
 import { useEffect } from 'react'
 import { fetchApi } from './utils/Api'
+import { getApiConfeliation } from './Store/HomeSlice'
+import { useDispatch,useSelector } from "react-redux";
 function App() {
+  const {url} = useSelector((state)=>state.Home)
+  console.log(url);
+  const dispath = useDispatch()
   useEffect(()=>{
 api()
   },[])
   const api = ()=>{
     fetchApi("/movie/popular")
     .then((res)=>{
-      console.log(res);
+      // console.log(res);
+      dispath(getApiConfeliation(res))
     })
   }
 
   return (
     <>
-      <h1>App</h1>
+      <h1>{url.total_pages}</h1>
     </>
   )
 }
