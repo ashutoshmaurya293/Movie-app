3c7f5253f016232cfc79b93477271e7d298c6b61
Ashutosh Maurya <ashutoshmaurya079@gmail.com>
2023-12-14 12:23:00 +0530

    making header section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,36 +4,36 @@ import { SlMenu } from "react-icons/sl";
 import { VscChromeClose } from "react-icons/vsc";
 import { useNavigate, useLocation } from "react-router-dom";
 
-import "./Header.css";
+import "./Header.scss";
 
 import ContentWrapper from "../contentWrapper/ContentWrapper";
 import logo from "../../assets/movix-logo.svg";
 
 const Header = () => {
-    const [show, setShow] = useState("top");
-    const [lastScrollY, setLastScrollY] = useState(0);
-    const [mobileMenu, setMobileMenu] = useState(false);
-    const [query, setQuery] = useState("");
-    const [showSearch, setShowSearch] = useState("");
-    const navigate = useNavigate();
-    const location = useLocation();
+  const [show, setShow] = useState("top");
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const [query, setQuery] = useState("");
+  const [showSearch, setShowSearch] = useState("");
+  const navigate = useNavigate();
+  const location = useLocation();
 
-    return (
-        <header>
-          <ContentWrapper>
-          <div className="log">
-            <img src={logo} alt="" />
-          </div>
-          <ul className="menuItem">
-            <li className="menuItem">Movie</li>
-            <li className="menuItem">Tv shows</li>
-            <li className="menuItem">
-              <HiOutlineSearch/>
-            </li>
-          </ul>
-          </ContentWrapper>
-        </header>
-    );
+  return (
+    <header className="header">
+      <ContentWrapper>
+        <div className="log">
+          <img src={logo} alt="" />
+        </div>
+        <ul className="menuItems">
+          <li className="menuItem">Movie</li>
+          <li className="menuItem">Tv shows</li>
+          <li className="menuItem">
+            <HiOutlineSearch />
+          </li>
+        </ul>
+      </ContentWrapper>
+    </header>
+  );
 };
 
 export default Header;
