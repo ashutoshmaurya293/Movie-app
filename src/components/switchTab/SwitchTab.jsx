61701f35dcdabe8e35c89252a797a000aa90edb9
Ashutosh Maurya <ashutoshmaurya079@gmail.com>
2023-12-15 11:52:56 +0530

    SwitchTab completed

--- src/components/switchTab/SwitchTab.jsx
+++ src/components/switchTab/SwitchTab.jsx
@@ -1,25 +1,29 @@
 import React, { useState } from "react";
-import "./SwitchTab.scss"
+import "./SwitchTab.scss";
 
 const SwitchTab = ({ data, onTabChange }) => {
-    const [selecated, setSelecated] = useState(0)
-    const [left, setLeft] = useState(0)
-    const activeTab = (tab, index) =>{
-      setLeft(index*100)
-      setTimeout(() => {
-        setSelecated(index)
-      }, 300);
-      onTabChange()
-    }
+  const [selecated, setSelecated] = useState(0);
+  const [left, setLeft] = useState(0);
+  const activeTab = (tab, index) => {
+    setLeft(index * 100);
+    setTimeout(() => {
+      setSelecated(index);
+    }, 300);
+    onTabChange();
+  };
   return (
     <div className="switchingTabs">
       <div className="tabItems">
         {data.map((tab, index) => (
-          <span key={index} className={`tabItem ${selecated == index? "active": ""}`} onClick={()=>activeTab(tab,index)}>
+          <span
+            key={index}
+            className={`tabItem ${selecated == index ? "active" : ""}`}
+            onClick={() => activeTab(tab, index)}
+          >
             {tab}
           </span>
         ))}
-        <span className="movingBg" style={{left}}/>
+        <span className="movingBg" style={{ left }} />
       </div>
     </div>
   );
