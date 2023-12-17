d73a7c6a730fea6ba28516a4470ba147685528e2
Ashutosh Maurya <ashutoshmaurya079@gmail.com>
2023-12-17 11:33:12 +0530

    Direction button added

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -19,67 +19,74 @@ const Carousel = ({ data, loading }) => {
   const { url } = useSelector((state) => state.Home);
   // console.log(url);
   const navigate = useNavigate();
-  const navigation = () => {};
+  const navigation = (dir) => {
+    const container = carouselConainer.current;
+    const scrollAmount =
+      dir === "left"
+        ? container.scrollLeft - (container.offsetWidth + 20)
+        : container.scrollLeft + (container.offsetWidth + 20);
+    container.scrollTo({
+      left: scrollAmount,
+      behavior: "smooth",
+    });
+  };
 
   const skItem = () => {
     return (
-        <div className="skeletonItem">
-            <div className="posterBlock skeleton"></div>
-            <div className="textBlock">
-                <div className="title skeleton"></div>
-                <div className="date skeleton"></div>
-            </div>
+      <div className="skeletonItem">
+        <div className="posterBlock skeleton"></div>
+        <div className="textBlock">
+          <div className="title skeleton"></div>
+          <div className="date skeleton"></div>
         </div>
+      </div>
     );
-};
-
+  };
 
   return (
     <div className="carousel">
       <ContentWrapper>
-      <BsFillArrowLeftCircleFill
-        className="carouselLeftNav arrow"
-        onClick={() => navigation("left")}
-      />
-      <BsFillArrowRightCircleFill
-        className="carouselRighttNav arrow"
-        onClick={() => navigation("right")}
-      />
-      {!loading ? (
-        <div className="carouselItems">
-          {data?.map((e) => {
-            // console.log(e);
-            const posterUrl = e.poster_path
-              ? url.poster + e.poster_path
-              : PosterFallback;
-            return (
-              <div className="carouselItem" key={e.id}>
-                <div className="posterBlock">
-                  <Image src={posterUrl} />
-                  <CircleRating rating={e.vote_average.toFixed(1)}/>
-                  <Genres  data={e.genre_ids.slice(0, 2)}/>
+        <BsFillArrowLeftCircleFill
+          className="carouselLeftNav arrow"
+          onClick={() => navigation("left")}
+        />
+        <BsFillArrowRightCircleFill
+          className="carouselRighttNav arrow"
+          onClick={() => navigation("right")}
+        />
+        {!loading ? (
+          <div className="carouselItems" ref={carouselConainer}>
+            {data?.map((e) => {
+              // console.log(e);
+              const posterUrl = e.poster_path
+                ? url.poster + e.poster_path
+                : PosterFallback;
+              return (
+                <div className="carouselItem" key={e.id}>
+                  <div className="posterBlock">
+                    <Image src={posterUrl} />
+                    <CircleRating rating={e.vote_average.toFixed(1)} />
+                    <Genres data={e.genre_ids.slice(0, 2)} />
+                  </div>
+                  <div className="textBlock">
+                    <span className="title">{e.title || e.name}</span>
+                    <span className="date">
+                      {dayjs(e.first_air_date).format("MMM D, YYYY")}
+                    </span>
+                  </div>
                 </div>
-                <div className="textBlock">
-                  <span className="title">
-                    {e.title || e.name}
-                  </span>
-                  <span className="date">
-                   {dayjs(e.first_air_date).format("MMM D, YYYY")}
-                  </span>
-                </div>
-              </div>
-            );
-          })}
-        </div>
-      ) : (
-       <div className="loadingSkeleton">
-        {skItem()}
-        {skItem()}
-        {skItem()}
-        {skItem()}
-        {skItem()}
-       </div>
-      )}
+              );
+            })}
+          </div>
+        ) : (
+          <div className="loadingSkeleton">
+            {skItem()}
+            {skItem()}
+            {skItem()}
+            {skItem()}
+            {skItem()}
+          </div>
+        )}
       </ContentWrapper>
     </div>
   );
