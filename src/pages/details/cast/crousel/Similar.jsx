db4f673a8879086a85a7484547452f21e4be1502
Ashutosh Maurya <ashutoshmaurya079@gmail.com>
2023-12-19 12:59:34 +0530

    Similar section added

---
+++ src/pages/details/cast/crousel/Similar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import Carousel from "../../../../components/carousel/Carousel";
+import Usefeth from "../../../../hooks/UseFetch";
+
+const Similar = ({ mediaType, id }) => {
+    const {data,loading} = Usefeth(`/${mediaType}/${id}/similar`)
+
+    const title = mediaType === "tv" ? "Similar TV Shows" : "Similar Movies";
+
+    return (
+        <Carousel
+            title={title}
+            data={data?.results}
+            loading={loading}
+            endpoint={mediaType}
+        />
+    );
+};
+
+export default Similar;
