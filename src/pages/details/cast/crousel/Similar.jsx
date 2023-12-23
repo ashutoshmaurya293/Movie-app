160706de66d0e88ec4021a2805b85971fb579287
Ashutosh Maurya <ashutoshmaurya079@gmail.com>
2023-12-23 20:07:02 +0530

    fixed bug in similar section

--- src/pages/details/cast/crousel/Similar.jsx
+++ src/pages/details/cast/crousel/Similar.jsx
@@ -4,18 +4,22 @@ import Carousel from "../../../../components/carousel/Carousel";
 import Usefeth from "../../../../hooks/UseFetch";
 
 const Similar = ({ mediaType, id }) => {
-    const {data,loading} = Usefeth(`/${mediaType}/${id}/similar`)
+  const { data, loading } = Usefeth(`/${mediaType}/${id}/similar`);
 
-    const title = mediaType === "tv" ? "Similar TV Shows" : "Similar Movies";
+  const title = mediaType === "tv" ? "Similar TV Shows" : "Similar Movies";
 
-    return (
+  return (
+    <>
+      {data?.results.length > 1 && (
         <Carousel
-            title={title}
-            data={data?.results}
-            loading={loading}
-            endPoints={mediaType}
+          title={title}
+          data={data?.results}
+          loading={loading}
+          endPoints={mediaType}
         />
-    );
+      )}
+    </>
+  );
 };
 
 export default Similar;
